601377642bb303b347ee7cafce46bd517926503d
winx <1577648502@qq.com>
2023-09-12 15:35:20 +0800

    项目初始化

--- src/main/java/com/lfg/ql_day1/QrDay1Application.java
+++ src/main/java/com/lfg/ql_day1/QrDay1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QlDay1Application {
+public class QrDay1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(QlDay1Application.class, args);
+        SpringApplication.run(QrDay1Application.class, args);
     }
 
 }
