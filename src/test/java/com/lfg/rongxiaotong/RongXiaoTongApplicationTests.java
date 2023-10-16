3c76a15f236053ceb5c0b7403c40153fb1312b56
winx <1577648502@qq.com>
2023-10-16 13:59:39 +0800

    update  专业知识，用户管理更新接口

--- src/test/java/com/lfg/rongxiaotong/RongXiaoTongApplicationTests.java
+++ src/test/java/com/lfg/rongxiaotong/RongXiaoTongApplicationTests.java
@@ -1,21 +1,14 @@
 package com.lfg.rongxiaotong;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lfg.rongxiaotong.domain.User;
 import com.lfg.rongxiaotong.service.UserService;
-import com.lfg.rongxiaotong.utius.R;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.List;
 
 @SpringBootTest
-class QlDay1ApplicationTests {
+class RongXiaoTongApplicationTests {
     @Resource
     private UserService userService;
   @Test
